95eb35187d8ce170957307343e46f7945abcdec8
ShishirShekhar <sspdav02@gmail.com>
2022-12-16 20:15:06 +0530

    day7

---
+++ Day7: Python Exam revision/list_re.py
@@ -0,0 +1,55 @@
+# lst1 = []
+# lst2 = list()
+
+# lst1 = [1, 2, 3, 4, 5]
+# print(lst1[0])
+# print(lst1[1])
+# print(lst1[2])
+# print(lst1[3])
+# print(lst1[4])
+
+# print(len(lst1))
+
+# random number lists
+# num = [1, 2, 4, 6, 7, 8, 9, 10, 64, 65, 43, 67]
+# print(len(num))
+# print(num[7])
+# print(num[10])
+
+# print(num[-3])
+
+
+# l1 = [1, 2, 3, 4, 5]
+# l2 = [6, 7, 8, 9, 10]
+# l3 = l1 + l2
+# l4 = l1 * 3
+
+# print(l3)
+# print(l4)
+
+# l1 = [1, 2, 3, 4, 5]
+# name = "Rahul"
+# l1.insert(3, 6)
+# print(l1)
+
+# print(l1[::2])
+# print(name[::-1])
+# print(name)
+
+# names = ["Tanushri", "shishir", "yash", "rehan"]
+
+# for i in range(len(names)):
+#     print(names[i])
+    
+# for i in names:
+#     print(i)
+
+# name = "shishir"
+# print(list(name))
+
+# matrix = [[1, 2, 3], 
+#           [4, 5, 6],
+#           [7, 8, 9]
+#           ]
+
+# print(matrix[1][1])
---
+++ Day7: Python Exam revision/st.py
@@ -0,0 +1,20 @@
+# ord and chr
+
+# print(ord('a'))
+# print(chr(65))
+
+# 65 A
+# 97 a
+
+std = {
+    1: "Rahul",
+    2: "Shishir",
+    3: "Tanushri",
+    4: "Yash",
+    5: "Rehan"
+}
+
+print(std.items())
+
+std[1] = "Rahul Kumar"
+std[6] = "Rahul's brother"
